623d3fe74ff474d7888bbae56a24ba9ff04592f1
YulianH <136604048+YulianH@users.noreply.github.com>
2023-07-07 11:54:23 +0200

    Add Blocked User Test

--- pages/Api.ts
+++ pages/Api.ts
@@ -5,6 +5,7 @@ import { ApiLeadersPage } from "./apiPages/leader_page"
 import { ApiDeleteAccountPage } from "./apiPages/deleteAccount_page"
 import { ApiStreamPage } from "./apiPages/stream_page"
 import { ApiGiftsPage } from "./apiPages/gifts_page"
+import { ApiBlockedPage } from "./apiPages/blocked_page"
 
 export class Api {
     apiContext: any
@@ -14,6 +15,7 @@ export class Api {
     deleteAccountPage: ApiDeleteAccountPage
     streamsPage: ApiStreamPage
     giftsPage: ApiGiftsPage
+    blockedPage: ApiBlockedPage
 
     constructor(apiContext: APIRequestContext) {
         this.apiContext = apiContext
@@ -22,6 +24,7 @@ export class Api {
         this.leadersPage = new ApiLeadersPage(apiContext)
         this.deleteAccountPage = new ApiDeleteAccountPage(apiContext)
         this.streamsPage = new ApiStreamPage(apiContext)
+        this.blockedPage = new ApiBlockedPage(apiContext)
         this.giftsPage = new ApiGiftsPage(apiContext)
     }
 }
