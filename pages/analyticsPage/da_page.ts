51a7d17696e0f5d234160084c4a7cc046f587ad1
KarinaPukas <84925072+KarinaPukas@users.noreply.github.com>
2023-07-13 13:31:49 +0300

    [Pb-1457] changes for analytics DA

--- pages/analyticsPage/da_page.ts
+++ pages/analyticsPage/da_page.ts
@@ -9,44 +9,44 @@ export class AnalyticsDeviceActivityPage {
         this.apiContext = apiContext
     }
 
-    async showRegModal(url: string, userToken: string, userId: string, platform: 'iOS' | 'Web' | 'Android' ) {
-        const apiContext = await request.newContext({ignoreHTTPSErrors: true})
+    async showRegModal(url: string, userToken: string, userId: string, platform: 'iOS' | 'Web' | 'Android') {
+        const apiContext = await request.newContext({ ignoreHTTPSErrors: true })
         const data = DaPayloads.showRegModal(userId, platform)
         const headers = Headers.userHeader(userToken)
 
-        const apiRequest = await apiContext.post(`${url}:3005/a/da`, {data, headers: headers})
+        const apiRequest = await apiContext.post(`${url}:3005/a/da`, { data, headers: headers })
         expect(apiRequest.status()).toEqual(200)
         console.log(`request for registration modal send, platform ${platform}`)
     }
 
-    async clickCloseReg(url: string, userToken: string, userId: string, platform: 'iOS' | 'Web' | 'Android' ) {
-        const apiContext = await request.newContext({ignoreHTTPSErrors: true})
+    async clickCloseReg(url: string, userToken: string, userId: string, platform: 'iOS' | 'Web' | 'Android') {
+        const apiContext = await request.newContext({ ignoreHTTPSErrors: true })
         const data = DaPayloads.clickCloseReg(userId, platform)
         const headers = Headers.userHeader(userToken)
 
-        const apiRequest = await apiContext.post(`${url}:3005/a/da`, {data, headers: headers})
+        const apiRequest = await apiContext.post(`${url}:3005/a/da`, { data, headers: headers })
         expect(apiRequest.status()).toEqual(200)
-        console.log(`request for registration modal send, platform ${platform}`)
+        console.log(`request for registration modal closed, platform ${platform}`)
     }
 
 
-    async clickStartReg(url: string, userToken: string, userId: string, platform: 'iOS' | 'Web' | 'Android' ) {
-        const apiContext = await request.newContext({ignoreHTTPSErrors: true})
+    async clickStartReg(url: string, userToken: string, userId: string, platform: 'iOS' | 'Web' | 'Android') {
+        const apiContext = await request.newContext({ ignoreHTTPSErrors: true })
         const data = DaPayloads.clickStartReg(userId, platform)
         const headers = Headers.userHeader(userToken)
 
-        const apiRequest = await apiContext.post(`${url}:3005/a/da`, {data, headers: headers})
+        const apiRequest = await apiContext.post(`${url}:3005/a/da`, { data, headers: headers })
         expect(apiRequest.status()).toEqual(200)
-        console.log(`request for registration modal send, platform ${platform}`)
+        console.log(`request for start registration, platform ${platform}`)
 
     }
 
-    async showJoinModal(url: string, userToken: string, userId: string, platform: 'Web' ) {
-        const apiContext = await request.newContext({ignoreHTTPSErrors: true})
+    async showJoinModal(url: string, userToken: string, userId: string, platform: 'Web') {
+        const apiContext = await request.newContext({ ignoreHTTPSErrors: true })
         const data = DaPayloads.clickStartReg(userId, platform)
         const headers = Headers.userHeader(userToken)
 
-        const apiRequest = await apiContext.post(`${url}:3005/a/da`, {data, headers: headers})
+        const apiRequest = await apiContext.post(`${url}:3005/a/da`, { data, headers: headers })
         expect(apiRequest.status()).toEqual(200)
         console.log(`request for registration modal send, platform ${platform}`)
 
