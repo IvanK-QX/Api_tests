b1ae30bc466d8700b943c90bd340bbcdc1b70a8e
IvanK-QX <ivan.k@unitedtech.ai>
2023-09-19 09:54:28 +0300

    precondition

--- tests/api_tests/draft_load_test.spec.ts
+++ tests/api_tests/draft_load_test.spec.ts
@@ -4,23 +4,23 @@ import { Api } from '../../pages/Api';
 import { apiUrl } from '../../utils/apiUrl';
 let user
 
-test.describe.skip('Get data', () => {
-// let userData = [0]
+test.describe.only('Get data', () => {
+let userData = [0]
 
-// for(let i = 1; i < 1000; i++) {
-//   userData.push(i)
-// }
-//   for (const name of userData) {
-//     test(`CRUD Group Post number ${name}`, async () => {
-//       const apiContext = await request.newContext()
-//       const api = new Api(apiContext)
-//       const myUserId = '648b24260642b2b48c2295da',
-//       giftId = '614c564a74faac3e988b2c98'
-//       user = await api.loginPage.createNewUser(apiUrl.qaEnvUrl)
-//       await api.followingPage.follow(apiUrl.qaEnvUrl, user.userToken, myUserId)
-//       await api.giftsPage.sendGiftShort(apiUrl.qaEnvUrl, user.userToken, giftId, myUserId)
-//       console.log(`Done test: ${name}`)
+for(let i = 1; i < 100000; i++) {
+  userData.push(i)
+}
+  for (const name of userData) {
+    test(`CRUD Group Post number ${name}`, async () => {
+      const apiContext = await request.newContext()
+      const api = new Api(apiContext)
+      const myUserId = '648b24260642b2b48c2295da',
+      giftId = '614c564a74faac3e988b2c98'
+      user = await api.loginPage.createNewUser(apiUrl.qaEnvUrl)
+      await api.followingPage.follow(apiUrl.qaEnvUrl, user.userToken, myUserId)
+    //   await api.giftsPage.sendGiftShort(apiUrl.qaEnvUrl, user.userToken, giftId, myUserId)
+      console.log(`Done test: ${name}`)
 
-//     })
-//   }
+    })
+  }
 })
