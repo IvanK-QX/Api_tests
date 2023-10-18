78fb2b61bc90d8cc95b6ae3870a03f70c35eb968
KarinaPukas <84925072+KarinaPukas@users.noreply.github.com>
2023-10-18 22:53:28 +0300

    cleanup query params

--- pages/analyticsPage/ua_payloads.ts
+++ pages/analyticsPage/ua_payloads.ts
@@ -1,16 +1,19 @@
 export class UaPayloads {
+  static defaultQueryParams = {
+    "brandId": 1,
+    "role": "user",
+    "appName": "plamfy",
+  };
 
   static showCreateAccountSuccess(userId: string, platform: string) {
     const query = [
       {
+        ...this.defaultQueryParams,
         "params": "{\"context\":\"Start Stream\",\"os\":\"iOS 16.1\",\"userRole\":\"user\",\"userId\":\"11307\",\"deviceType\":\"iPhone XR\",\"anchor\":\"start_stream\",\"method\":\"google\"}",
         "deviceId": "6817C448-897A-45ED-A889-4C249D0C1BF9",
-        "brandId": 1,
         "userId": `${userId}`,
-        "role": "user",
         "event": "show_create_account_success",
         "platform": `${platform}`,
-        "appName": "plamfy",
         "eventTimeMs": 1689150057862,
         "idfv": "BAC5C2CB-82E8-4BF0-822E-507A6F4D4F10",
         "context": "Start Stream"
@@ -22,14 +25,12 @@ export class UaPayloads {
   static pageviewFollowTop(userId: string, platform: string) {
     const query = [
       {
+        ...this.defaultQueryParams,
         "params": "{\"context\":\"Splash\",\"os\":\"iOS 16.1\",\"userRole\":\"user\",\"deviceType\":\"iPhone XR\"}",
         "deviceId": "6817C448-897A-45ED-A889-4C249D0C1BF9",
-        "brandId": 1,
         "userId": `${userId}`,
-        "role": "user",
         "event": "pageview_follow_top",
         "platform": `${platform}`,
-        "appName": "plamfy",
         "eventTimeMs": 1689080342034,
         "idfv": "BAC5C2CB-82E8-4BF0-822E-507A6F4D4F10",
         "context": "Splash"
